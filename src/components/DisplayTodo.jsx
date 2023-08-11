b959a82884cdaf369eb34fa885dd9fb7e1a73bed
Nishan9t <pandeynisha9t@gmail.com>
2023-08-11 12:38:10 +0530

    displayTodo frontend is completed

--- src/components/DisplayTodo.jsx
+++ src/components/DisplayTodo.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Container,Row,Col,Card,ListGroup} from 'react-bootstrap'
 
 const DisplayTodo = () => {
+
+    const [todos,setTodos]=useState([
+        {
+        title:'first title',
+        description:"first desc"
+         },
+        {
+        title:'second title',
+        description:"second desc"
+         },
+        {
+        title:'third title',
+        description:"third desc"
+         }
+        ])
   return (
-    <div>DisplayTodo</div>
+    <Container>
+        <Row>
+            <Col>
+                <Card className='mt-3 shadow-sm'>
+                    <Card.Body>
+                        <h3> all todos are here</h3>
+                        {
+                            todos.map((todo,index)=>(
+
+                                <ListGroup.Item key={index}>
+                                <h4>{todo.title}</h4>
+                                <p>{todo.description}</p>
+                                </ListGroup.Item>
+                            )
+
+                            )
+                        }
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+    </Container>
   )
 }
 
